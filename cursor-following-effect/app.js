b18cb2a6405596b796d16018dd02fff11f4fd660
ln_north <yamahisa.kita@gmail.com>
2019-10-28 04:13:51 +0900

    webglをmdnに従ってなんとかチュートリアルしたつもり

--- cursor-following-effect/app.js
+++ cursor-following-effect/app.js
@@ -1,6 +1,21 @@
 const cursorPosition = document.querySelector('.cursor-position');
 
-window.addEventListener('mousemove', (event) => {
-  cursorPosition.style.left = event.clientX + 'px';
-  cursorPosition.style.top = event.clientY + 'px';
-});
+let ticking = false;
+function optimizeEventAction(event, func, ...args) {
+  if (!ticking) {
+    requestAnimationFrame(() => {
+      ticking = false;
+      cursorPosition.style.left = event.clientX + 'px';
+      cursorPosition.style.top = event.clientY + 'px';
+    });
+    ticking = true;
+  }
+}
+
+window.addEventListener('mousemove', optimizeEventAction, {passive: true});
+
+const adjustDurationRange = document.querySelector('.adjust-duration');
+adjustDurationRange.addEventListener('input', () => {
+  adjustDurationRange.setAttribute('data-duration', adjustDurationRange.value);
+})
+adjustDurationRange.setAttribute('data-duration', adjustDurationRange.value);
